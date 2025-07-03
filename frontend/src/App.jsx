bb9bc20d85086edf5ca2bfce44577cf71cde3ce4
Dineshkanna.R <dineshkanna8072@gmail.com>
2025-07-03 10:56:14 +0530

    added seprate routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,11 +19,25 @@ export default function App() {
     <div>
       <h2>Attendance Management System</h2>
       <nav>
-        <Link to="/register">Register</Link> |{" "}
-        <Link to="/login">Login</Link> |{" "}
-        <Link to="/attendance">View Attendance</Link> |{" "}
-        {userId && <button onClick={handleLogout}>Logout</button>}
+        
+        
       </nav>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link class="nav-link active" to="/register">Register</Link> |{" "}
+              <Link class="nav-link" to="/login">Login</Link> |{" "}
+              <Link class="nav-link" to="/attendance">View Attendance</Link> |{" "}
+              {userId &&<button onClick={handleLogout} type="button" class="btn btn-info">Logout</button>}
+            </div>
+           </div>
+         </div>
+        </nav>
       <hr />
       <Outlet context={{ setUserId }} />
     </div>
